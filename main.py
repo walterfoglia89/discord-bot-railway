762dc825300b3ac6c2e8129e17a4f2cef45ed0b1
Walter <Walter.foglia@gmail.com>
2025-04-05 14:05:51 +0200

    Aggiunto bot base discord

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+import os
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"Bot connesso come {client.user}")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.lower() == "!ping":
+        await message.channel.send("Pong!")
+
+client.run(TOKEN)
